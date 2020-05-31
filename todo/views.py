3281992d3e1cae8f58fa2645d61d35df735f1439
Soliman <solieman.elsaber@gmail.com>
2020-06-01 00:00:09 +0800

    Adding the model. Got some insights from this article https://medium.com/fbdevclagos/how-to-build-a-todo-app-with-django-17afdc4a8f8c

--- todo/views.py
+++ todo/views.py
@@ -1,16 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import TodoList, Category
 
 # Create your views here.
 
 
 def index(request):
+    todos = TodoList.objects.all()  # quering all todos with the object manager
     return render(request, 'hello.html')
 
 
 def todo_list(request):
     text = """<h1>Building awesome to-do list!</h1>"""
+    return HttpResponse(text)
+
+
+def todo_task(request):
+    text = """<div class="row">
+                            <button class="taskDelete" name="taskDelete" formnovalidate="" type="submit"
+                                    onclick="$('input#sublist').click();"><i class="fa fa-trash-o icon"></i>Delete Tasks
+                            </button>
+                        </div>"""
     return HttpResponse(text)
 
 
@@ -23,6 +34,3 @@ def hello_parameter(request, number):
     text = "<h1>welcome to my app number %s!</h1>" % number
     return HttpResponse(text)
 
-
-def hello_template(request):
-    return render(request, "todo_list/template/hello.html", {})
