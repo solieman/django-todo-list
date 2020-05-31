85147bd9d99b39656adc5fd8cfcb942abb4fa7f4
Soliman <solieman.elsaber@gmail.com>
2020-06-01 00:51:37 +0800

    Fixing DB and activate Add task button

--- todo/views.py
+++ todo/views.py
@@ -1,14 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
-from .models import TodoList, Category
+from .models import TodoList
+
 
 # Create your views here.
 
 
 def index(request):
     todos = TodoList.objects.all()  # quering all todos with the object manager
-    return render(request, 'hello.html')
+
+    if request.method == "POST":  # checking if the request method is a POST
+        if "taskAdd" in request.POST:  # checking if there is a request to add a todo
+            title = request.POST["description"]  # title
+            Todo = TodoList(title=title)
+            Todo.save()  # saving the todo
+            return redirect("/")  # reloading the page
+        if "taskDelete" in request.POST:  # checking if there is a request to delete a todo
+            checkedlist = request.POST["checkedbox"]  # checked todos to be deleted
+            for todo_id in checkedlist:
+                todo = TodoList.objects.get(id=int(todo_id))  # getting todo id
+                todo.delete()  # deleting todo
+    return render(request, "hello.html", {"todos": todos})
 
 
 def todo_list(request):
@@ -33,4 +46,3 @@ def hello_view(request):
 def hello_parameter(request, number):
     text = "<h1>welcome to my app number %s!</h1>" % number
     return HttpResponse(text)
-
